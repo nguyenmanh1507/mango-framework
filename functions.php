1d5357b326ee78ff15728f5d70c20bea6ba9e561
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-04-18 11:49:14 +0700

    add skeleton code

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * functions.php
+ *
+ * The theme's functions and definitions.
+ */
+
+/**
+ * -----------------------------------------------------------------------------
+ * 1.0 - Define constants.
+ * -----------------------------------------------------------------------------
+ */
+define( 'THEMEROOT', get_stylesheet_directory_uri() );
+define( 'IMAGES', THEMEROOT . '/images' );
+define( 'SCRIPTS', THEMEROOT . '/js' );
+define( 'FRAMEWORK', get_template_directory() .'/framework' );
+define( 'TEXTDOMAIN', 'mango' );
+
+/**
+ * -----------------------------------------------------------------------------
+ * 2.0 - Load the framework.
+ * -----------------------------------------------------------------------------
+ */
+require_once( FRAMEWORK . '/init.php' );
+
+/**
+ * -----------------------------------------------------------------------------
+ * 3.0 - Set up the content width value based on the theme'design.
+ * -----------------------------------------------------------------------------
+ */
+if ( !isset( $content_width ) ) {
+  $content_width = 800;
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * 4.0 - Set up theme default and register various supported features.
+ * -----------------------------------------------------------------------------
+ */
+if ( !function_exists( 'mango_setup' ) ) {
+  function mango_setup() {
+    /**
+     * Make the theme available for translation.
+     */
+    $lang_dir = THEMEROOT . '/languages';
+    load_theme_textdomain( TEXTDOMAIN, $lang_dir );
+
+    /**
+     * Add support for post format.
+     */
+    add_theme_support( 'post-formats', array(
+      'gallery',
+      'link',
+      'image',
+      'quote',
+      'video',
+      'audio'
+    ) );
+
+    /**
+     * Add support for automatic feed links.
+     */
+    add_theme_support( 'automatic-feed-links' );
+
+    /**
+     * Add support for post thumbnails.
+     */
+    add_theme_support( 'post-thumbnails' );
+
+    /**
+     * Register nav menus.
+     */
+    register_nav_menus(
+      array(
+        'main-menu' => __( 'Main Menu', TEXTDOMAIN )
+      )
+    );
+  }
+  add_action( 'after_theme_setup', 'alpha_setup' );
+}
