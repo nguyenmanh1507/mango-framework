44e39ce3235aef7ac1b34ff23eb9862f14af0b70
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-04-19 14:43:22 +0700

    .\template-parts\content-link.php

---
+++ template-parts/content-link.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * content-link.php
+ *
+ * The default template for displaying post with the Link post format.
+ */
+?>
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+  <div class="entry-content">
+    <?php
+    the_content( __( 'Continue reading &rarr;', TEXTDOMAIN ) );
+    wp_link_pages();
+    ?>
+  </div>
+
+  <footer class="entry-footer">
+    <div class="entry-meta">
+      <?php alpha_post_meta(); ?>
+    </div>
+    <?php
+    // If we have a single page and the author bio exists,
+    // display it
+    if ( is_single() && get_the_author_meta( 'description' ) ) {
+      echo '<h2>' . __( 'Written by', TEXTDOMAIN ) . get_the_author() . '</h2>';
+      echo '<p>' . get_the_author_meta( 'description' ) . '</p>';
+    }
+    ?>
+  </footer>
+</article>
