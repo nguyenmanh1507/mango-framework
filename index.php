c37e36dc414e45483d9723f32c4bef78a9f6d3ae
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-04-19 15:28:59 +0700

    include header sidebar and footer

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 * The main template file.
 */
 ?>
+<?php get_header(); ?>
+
 <main class="main-content" role="main">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
@@ -16,3 +18,6 @@
   <?php endif; ?>
 </main>
 <!-- /.main-content -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
