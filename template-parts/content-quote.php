fe59b7278cec4b1945d6f0e1a8888b525b8f15b7
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-04-19 14:45:01 +0700

    create content quote

---
+++ template-parts/content-quote.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * content-quote.php
+ *
+ * The default template for displaying post with the Quote post format.
+ */
+?>
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+  <div class="entry-content">
+    <?php
+    the_content( __( 'Continue reading &rarr;', TEXTDOMAIN ) );
+    wp_link_pages();
+    ?>
+  </div>
+
+  <footer class="entry-footer">
+    <div class="entry-meta">
+      <?php alpha_post_meta(); ?>
+    </div>
+  </footer>
+</article>
